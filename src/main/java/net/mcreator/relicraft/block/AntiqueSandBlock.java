f9f0ddddf407e262dc67f36bb80b2b8adf637943
relicraft.mode@gmail.com <Mathis@192.168.1.36>
2021-09-03 13:17:16 +0200

    sable antique

---
+++ src/main/java/net/mcreator/relicraft/block/AntiqueSandBlock.java
@@ -0,0 +1,64 @@
+
+package net.mcreator.relicraft.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.FallingBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.relicraft.itemgroup.RelicraftItemGroup;
+import net.mcreator.relicraft.RelicraftModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@RelicraftModElements.ModElement.Tag
+public class AntiqueSandBlock extends RelicraftModElements.ModElement {
+	@ObjectHolder("relicraft:antique_sand")
+	public static final Block block = null;
+	public AntiqueSandBlock(RelicraftModElements instance) {
+		super(instance, 19);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(RelicraftItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends FallingBlock {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.SAND).sound(SoundType.SAND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).harvestLevel(1)
+					.harvestTool(ToolType.SHOVEL).setRequiresTool().speedFactor(0.8f).jumpFactor(0.9f));
+			setRegistryName("antique_sand");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public MaterialColor getMaterialColor() {
+			return MaterialColor.SAND;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
