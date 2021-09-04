2dd66c5b9a3a52e908b46e7b3484cd9b3b4a85ed
relicraft.mode@gmail.com <Mathis@192.168.1.36>
2021-09-04 20:25:36 +0200

    cacti

--- src/main/java/net/mcreator/relicraft/block/InfusedCactusBlock.java
+++ src/main/java/net/mcreator/relicraft/block/InfusedCactusBlock.java
@@ -2,36 +2,18 @@
 package net.mcreator.relicraft.block;
 
 import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.common.ToolType;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
-import net.minecraft.world.gen.feature.template.RuleTest;
-import net.minecraft.world.gen.feature.template.IRuleTestType;
-import net.minecraft.world.gen.feature.OreFeatureConfig;
-import net.minecraft.world.gen.feature.OreFeature;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.World;
-import net.minecraft.world.ISeedReader;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.util.registry.WorldGenRegistries;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.RegistryKey;
 import net.minecraft.util.Direction;
 import net.minecraft.pathfinding.PathNodeType;
 import net.minecraft.item.ItemStack;
@@ -44,20 +26,15 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.PushReaction;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
 import net.mcreator.relicraft.procedures.DryCactusNeighbourBlockChangesProcedure;
-import net.mcreator.relicraft.procedures.DryCactusAdditionalGenerationConditionProcedure;
 import net.mcreator.relicraft.itemgroup.RelicraftItemGroup;
 import net.mcreator.relicraft.RelicraftModElements;
 
-import java.util.Random;
 import java.util.Map;
 import java.util.HashMap;
-
-import com.google.common.collect.ImmutableMap;
 
 @RelicraftModElements.ModElement.Tag
 public class InfusedCactusBlock extends RelicraftModElements.ModElement {
@@ -65,8 +42,6 @@ public class InfusedCactusBlock extends RelicraftModElements.ModElement {
 	public static final Block block = null;
 	public InfusedCactusBlock(RelicraftModElements instance) {
 		super(instance, 24);
-		MinecraftForge.EVENT_BUS.register(this);
-		FMLJavaModLoadingContext.get().getModEventBus().register(new FeatureRegisterHandler());
 	}
 
 	@Override
@@ -152,59 +127,5 @@ public class InfusedCactusBlock extends RelicraftModElements.ModElement {
 				DryCactusNeighbourBlockChangesProcedure.executeProcedure($_dependencies);
 			}
 		}
-	}
-	private static Feature<OreFeatureConfig> feature = null;
-	private static ConfiguredFeature<?, ?> configuredFeature = null;
-	private static IRuleTestType<CustomRuleTest> CUSTOM_MATCH = null;
-	private static class CustomRuleTest extends RuleTest {
-		static final CustomRuleTest INSTANCE = new CustomRuleTest();
-		static final com.mojang.serialization.Codec<CustomRuleTest> codec = com.mojang.serialization.Codec.unit(() -> INSTANCE);
-		public boolean test(BlockState blockAt, Random random) {
-			boolean blockCriteria = false;
-			if (blockAt.getBlock() == Blocks.AIR)
-				blockCriteria = true;
-			return blockCriteria;
-		}
-
-		protected IRuleTestType<?> getType() {
-			return CUSTOM_MATCH;
-		}
-	}
-
-	private static class FeatureRegisterHandler {
-		@SubscribeEvent
-		public void registerFeature(RegistryEvent.Register<Feature<?>> event) {
-			CUSTOM_MATCH = Registry.register(Registry.RULE_TEST, new ResourceLocation("relicraft:infused_cactus_match"), () -> CustomRuleTest.codec);
-			feature = new OreFeature(OreFeatureConfig.CODEC) {
-				@Override
-				public boolean generate(ISeedReader world, ChunkGenerator generator, Random rand, BlockPos pos, OreFeatureConfig config) {
-					RegistryKey<World> dimensionType = world.getWorld().getDimensionKey();
-					boolean dimensionCriteria = false;
-					if (dimensionType == World.OVERWORLD)
-						dimensionCriteria = true;
-					if (!dimensionCriteria)
-						return false;
-					int x = pos.getX();
-					int y = pos.getY();
-					int z = pos.getZ();
-					if (!DryCactusAdditionalGenerationConditionProcedure.executeProcedure(ImmutableMap.of("x", x, "y", y, "z", z, "world", world)))
-						return false;
-					return super.generate(world, generator, rand, pos, config);
-				}
-			};
-			configuredFeature = feature.withConfiguration(new OreFeatureConfig(CustomRuleTest.INSTANCE, block.getDefaultState(), 4)).range(90)
-					.square().func_242731_b(10);
-			event.getRegistry().register(feature.setRegistryName("infused_cactus"));
-			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("relicraft:infused_cactus"), configuredFeature);
-		}
-	}
-	@SubscribeEvent
-	public void addFeatureToBiomes(BiomeLoadingEvent event) {
-		boolean biomeCriteria = false;
-		if (new ResourceLocation("relicraft:antiquedesert").equals(event.getName()))
-			biomeCriteria = true;
-		if (!biomeCriteria)
-			return;
-		event.getGeneration().getFeatures(GenerationStage.Decoration.UNDERGROUND_ORES).add(() -> configuredFeature);
 	}
 }
